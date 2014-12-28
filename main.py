bd3bbec8063869945f35b0e4042cdf67232dba21
ender405 <laslack@gmail.com>
2014-12-27 17:07:35 -0700

    Build scaffold of app, to'dos noted

--- main.py
+++ main.py
@@ -15,26 +15,37 @@
 # limitations under the License.
 #
 import webapp2
-from validation import valid_month, valid_day, valid_year, escape_html
+import cgi
+from validation import valid_month, valid_day, valid_year, escape_html, valid_password, valid_username, valid_email, valid_verify
+
 
 
 form = """
 <form method="post">
-	What is your birthday?
+	Please enter your details
 	<br>
 	<label>
-		Month
-		<input type="text" name="month" value="%(month)s">
+		Username
+		<input type="text" name="username" value="%(username)s">
+		<div style="color: red">%(error_username)s</div>
 	</label>
 	<label>
-		Day
-		<input type="text" name="day" value="%(day)s">
+		Password
+		<input type="password" name="password" value="%(password)s">
+		<div style="color: red">%(error_password)s</div>
 	</label>
 	<label>
-		Year
-		<input type="text" name="year" value="%(year)s">
+		Verify password
+		<input type="password" name="verify" value="%(verify)s">
+		<div style="color: red">%(error_verify)s</div>
 	</label>
-	<div style="color: red">%(error)s</div>	
+	<label>
+		Email (optional)
+		<input type="text" name="email" value="%(email)s">
+		<div style="color: red">%(error_email)s</div>	
+	</label>
+
+	
 	<br>
 	<br>
 	<input type="submit">
@@ -43,33 +54,67 @@ form = """
 
 class MainHandler(webapp2.RequestHandler):
     
-    def write_form(self, error="", month="",day="",year=""):
-    	self.response.out.write(form % {"error" : error, "month" : escape_html(month), "day" : escape_html(day), "year" : escape_html(year)})
+    def write_form(self, 
+    				username="", 
+    				password="", 
+    				verify="", 
+    				email="", 
+    				error_username="", 
+    				error_password="", 
+    				error_verify="", 
+    				error_email=""):
+    	
+    	self.response.out.write(form % {"username" : escape_html(username), 
+    									"password" : "", 
+    									"verify" : "",
+    									"email" : escape_html(email),
+    									"error_username" : error_username,
+    									"error_password": error_password,
+    									"error_verify" : error_verify,
+    									"error_email" : error_email,
+    									})
 
     def get(self):
 #        self.response.headers['Content-type'] = 'text/plain'
         self.write_form()
 
+
+# need to fix the validation steps and figure out what to do with a null email
+# need to figure out how to pass username into the redicrect to display names
+
     def post(self):
 
-    	user_month = self.request.get('month')
-    	user_day = self.request.get('day')
-    	user_year = self.request.get('year')
+    	entry_username = self.request.get('username')
+    	entry_password = self.request.get('password')
+    	entry_verify = self.request.get('verify')
+    	entry_email = self.request.get('email')
 
-    	month = valid_month(user_month)
-    	day = valid_day(user_day)
-    	year = valid_year(user_year)
 
-    	if not (month and day and year):
-    		self.write_form("That doesn't look valid to me, friend", user_month, user_day, user_year)
+    	username = valid_username(entry_username)
+    	password = valid_password(entry_password)
+    	verify = valid_verify(entry_password, entry_verify)
+    	email = valid_email(entry_email)
+
+    	if (username and password and email):
+    		self.redirect("/welcome")
     	else:
-    		self.redirect("/thanks")
+    		errors = ["", "", "", ""]
+    		if not username:
+    			errors[0] = "That's not a valid username."
+    		if not password:
+    			errors[1] = "That wasn't a valid password"
+    		if not verify:
+    			errors[2] = "Your two passwords don't match"
+    		if not email:
+    			errors[3] = "Your email isn't in the correct format"
+    		self.write_form(entry_username, "", "", entry_email, errors[0], errors[1], errors[2], errors[3])
 
-class ThanksHandler(webapp2.RequestHandler):
+class WelcomeHandler(webapp2.RequestHandler):
 	def get(self):
-		self.response.out.write("Thanks! That's a totally valid birthday!")
+		username = self.request.get('username')
+		self.response.out.write("Welcome " + username + "!")
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
-    ('/thanks', ThanksHandler)
+    ('/welcome', WelcomeHandler)
 ], debug=True)
--- validation.py
+++ validation.py
@@ -21,9 +21,32 @@
 
 # my answers
 import cgi
+import re
 
 def escape_html(s):
   return cgi.escape(s, quote = True)
+
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+def valid_username(username):
+  return USER_RE.match(username)
+
+PASSWORD_RE = re.compile(r"^.{3,20}$")
+def valid_password(password):
+  return PASSWORD_RE.match(password)
+
+def valid_verify(password, verify):
+  if password == verify:
+    return verify
+  else:
+    return None
+
+EMAIL_RE = re.compile(r"^[\S]+@[\S]+\.[\S]+$")
+def valid_email(email):
+  if email == "":
+    return email
+  else:
+    return EMAIL_RE.match(email)
+
 
 def valid_month(month):
     months = ['January',
